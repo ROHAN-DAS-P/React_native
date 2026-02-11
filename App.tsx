bcfff79fa0a83448c1919ce7a238e119e106fb05
ROHAN DAS P <rohan2003710@gmail.com>
2026-02-11 20:26:33 +0530

    modified main app.tsx

--- App.tsx
+++ App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { View, StatusBar, StyleSheet, Text } from 'react-native';
+import { AuthProvider } from './src/auth/AuthContext';
+import { TaskProvider } from './src/tasks/TaskContext';
+import AuthNavigator from './src/navigation/AuthNavigator';
 
-
-const App: React.FC = () => {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text style={styles.title}>Todo App</Text>
-    </View>
+    <AuthProvider>
+      <TaskProvider>
+        <AuthNavigator />
+      </TaskProvider>
+    </AuthProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-});
 
 export default App;
